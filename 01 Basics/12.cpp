b939d317539412783991d9913611b546e65f9097
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 21:10:17 +0530

    Square of a number user input

---
+++ 01 Basics/12.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  cout << "Enter a number: ";
+  // Use cin to read the input value
+  cin >> x;
+
+  cout << "The square of the number is: " << x * x << endl;
+}
