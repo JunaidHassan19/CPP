e0e5880267c5e8d3aab63b1229592043299b860b
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 19:28:32 +0530

    variable

---
+++ 01 Basics/01-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;  // Declare an integer variable x
+  x = 10; // Assign 10 to x
+  cout << x << endl
+       << endl;
+
+  int y;  // Declare an integer variable y
+  y = 20; // Assign 20 to y
+  cout << y << endl
+       << endl;
+  cout << x + y << endl // Print the sum of x and y
+       << endl;
+
+  int z = 19; // Declare and initialize an integer variable z with 19
+  cout << z << endl
+       << endl;
+  cout << x + y + z << endl // Print the sum of x, y, and z
+       << endl;
+}
