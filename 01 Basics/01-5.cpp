99023539e0787111cc161dceeab6d537d3bee680
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 19:47:27 +0530

    Arithmetic operations on int data type

---
+++ 01 Basics/01-5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x = 10;
+  int y = 3;
+
+  cout << x + y << endl; // Output the sum of x and y (13)
+  cout << x - y << endl; // Output the difference of x and y (7)
+  cout << x * y << endl; // Output the product of x and y (30)
+  cout << x / y << endl; // Output the quotient of x and y (3)
+}
