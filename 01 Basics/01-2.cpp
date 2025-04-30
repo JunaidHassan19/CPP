63b350114e71083d7eedf92127aec6384647aa3d
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 19:11:29 +0530

    Print a Number

---
+++ 01 Basics/01-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>;
+using namespace std;
+
+int main()
+{
+  cout << "19"; // Print "19" (as a string)
+  cout << endl
+       << endl;
+
+  cout << 19; // Print 19 (as a number)
+  cout << endl
+       << endl;
+
+  cout << "10 + 9"; // Print "10 + 9" (as a string)
+  cout << endl
+       << endl;
+
+  cout << 10 + 9; // Print 19 (as a number)
+  cout << endl
+       << endl;
+}
