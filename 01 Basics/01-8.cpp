b2948a8d8b4c58e622c41120bd6cb74000c984a6
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 20:21:19 +0530

    Area of a circle

---
+++ 01 Basics/01-8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float r = 5.0;             // radius
+  float area = 3.14 * r * r; // area of circle
+
+  cout << "The area of the circle with radius " << r << " is: " << area << endl;
+}
