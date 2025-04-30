c9244a71bb00d0bc66fd571e704c5ce7e901d924
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 19:58:12 +0530

    Arithmetic operations on float data type

---
+++ 01 Basics/01-6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // float x = 3.14;
+  // float y = 2.71;
+  float x = 10;
+  float y = 3;
+
+  cout << x + y << endl; // Add x and y and print the result
+  cout << x - y << endl; // Subtract y from x and print the result
+  cout << x * y << endl; // Multiply x and y and print the result
+  cout << x / y << endl; // Divide x by y and print the result
+}
