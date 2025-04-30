62da369905d85ae53433c442e882957607e41059
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 21:14:41 +0530

    Sum  of a number user input

---
+++ 01 Basics/13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  cout << "Enter a number: ";
+  cin >> x;
+
+  int y;
+  cout << "Enter another number: ";
+  cin >> y;
+
+  int sum = x + y;
+  cout << "The sum is: " << sum << endl;
+}
