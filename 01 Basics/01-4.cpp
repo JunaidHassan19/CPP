35df7cbc53bfd2677fe6bd8ba1c4eda7bc08ca67
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 19:39:21 +0530

    Updation of Variables

---
+++ 01 Basics/01-4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x = 10;
+  cout << x << endl;
+
+  x = 9; // update x to 9
+  cout << x << endl;
+
+  x = x + 5; // update x to x + 5 (14)
+  cout << x << endl;
+
+  x = x - 2; // update x to x - 2 (12)
+  cout << x << endl;
+
+  x = x * 3; // update x to x * 3 (36)
+  cout << x << endl;
+}
