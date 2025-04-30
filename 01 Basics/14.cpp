465c9de98fb3c9b59cc58ab1e09823f0f43a3103
JunaidHassan19 <thejunni01@gmail.com>
2025-04-30 21:17:09 +0530

    Sum of 3 number user input

---
+++ 01 Basics/14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  cout << "Enter 1st number: ";
+  cin >> x;
+
+  int y;
+  cout << "Enter 2nd number: ";
+  cin >> y;
+
+  int z;
+  cout << "Enter 3rd number: ";
+  cin >> z;
+
+  int sum = x + y + z;
+  cout << "The sum is: " << sum << endl;
+}
