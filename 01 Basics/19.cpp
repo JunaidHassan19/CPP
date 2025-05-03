083372ab7df6436df7149fdb82a1b892cb34291a
JunaidHassan19 <thejunni01@gmail.com>
2025-05-03 14:37:15 +0530

    ASCII values

---
+++ 01 Basics/19.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  char x;
+  cout << "Enter a character: ";
+  cin >> x;
+  cout << "ASCII value of " << x << " is " << int(x) << endl;
+}
+
+/*
+
+ASCII values
+a = 97   A = 65  '0' = 48
+b = 98   B = 66  '1' = 49
+c = 99   C = 67  '2' = 50
+d = 100  D = 68  '3' = 51
+e = 101  E = 69  '4' = 52
+f = 102  F = 70  '5' = 53
+g = 103  G = 71  '6' = 54
+h = 104  H = 72  '7' = 55
+i = 105  I = 73  '8' = 56
+j = 106  J = 74  '9' = 57
+k = 107  K = 75
+l = 108  L = 76
+m = 109  M = 77
+n = 110  N = 78
+o = 111  O = 79
+p = 112  P = 80
+q = 113  Q = 81
+r = 114  R = 82
+s = 115  S = 83
+t = 116  T = 84
+u = 117  U = 85
+v = 118  V = 86
+w = 119  W = 87
+x = 120  X = 88
+y = 121  Y = 89
+z = 122  Z = 90
+*/
