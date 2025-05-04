09c0edd13f35dec65300ca1269f68452bdf2507c
JunaidHassan19 <thejunni01@gmail.com>
2025-05-04 17:09:15 +0530

    for loop (print the table of 19)

---
+++ 03 Loops/06.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // for (int i = 19; i <= 200; i++)
+  // {
+  //   if (i % 19 == 0)
+  //   {
+  //     cout << i << " ";
+  //   }
+  // }
+
+  for (int i = 19; i <= 200; i += 19)
+  {
+    cout << i << " ";
+  }
+}
