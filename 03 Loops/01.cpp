e62bfff704ba3ec71b3a38d8a740d996d889c9eb
JunaidHassan19 <thejunni01@gmail.com>
2025-05-04 16:29:38 +0530

    for loop (prints "Hello world!" 10 times)

---
+++ 03 Loops/01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // This program prints "Hello world!" 10 times using a for loop
+  for (int i = 0; i <= 10; i++)
+  {
+    cout << "Hello world!" << endl;
+  }
+}
