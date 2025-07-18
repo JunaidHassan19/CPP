3b1eec3ac57877897a6034887c5c5365ccc81cd3
JunaidHassan19 <thejunni01@gmail.com>
2025-07-18 19:35:19 +0530

    Number Pyramid

--- 04 Pattern Printing/29.cpp
+++ 04 Pattern Printing/29.cpp
@@ -11,9 +11,9 @@ int main()
   {
     for (int j = 1; j <= n - i; j++)
     {
-      cout << " ";
+      cout << "  ";
     }
-    for (int j = 1; j <= i; j++)
+    for (int j = 1; j <= 2 * i - 1; j++)
     {
       cout << j << " ";
     }
