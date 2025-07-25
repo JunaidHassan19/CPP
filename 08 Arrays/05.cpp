9bd66c76af0f5eb7fa249ce189027d888a6be7bb
JunaidHassan19 <thejunni01@gmail.com>
2025-07-25 15:26:09 +0530

    Calculate the sum of all the elements in the given array

---
+++ 08 Arrays/05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Q: Calculate the sum of all the elements in the given array.
+
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int n = sizeof(arr) / 4;
+  int sum = 0;
+  for (int i = 0; i <= n; i++)
+  {
+
+    sum += arr[i];
+  }
+  cout << "The sum of the Elements is: " << sum;
+}
