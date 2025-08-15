1e35b736c692c2eac90f86bef2db719cd92d56e2
JunaidHassan19 <thejunni01@gmail.com>
2025-08-15 11:54:29 +0530

    Reverse string

--- 10 Strings/08.cpp
+++ 10 Strings/08.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main()
 {
+  // Initialize a std::string with a literal
   string str = "Junni";
-  cout << str << endl;
+  // Print the original string
+  cout << str << endl; // Output: Junni
+
+  // reverse() from <algorithm> reverses the range in-place.
+  // here we pass iterators to the beginning and one-past-the-end
+  // of the string: str.begin() and str.end().
   reverse(str.begin(), str.end());
-  cout << str << endl;
+
+  // Print the reversed string
+  cout << str << endl; // Output: inniJ
 }
